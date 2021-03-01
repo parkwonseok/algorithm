38fc53a181a2b8c968f473e2a6b3298b5baa3ba6
parkwonseok <48585170+parkwonseok@users.noreply.github.com>
2021-03-01 20:23:03 +0900

    Day7

--- DFS & BFS/BOJ_13459_구슬탈출.java
+++ DFS & BFS/BOJ_13459_구슬탈출.java
@@ -5,7 +5,7 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.StringTokenizer;
 
-public class BOJ_13459_ {
+public class BOJ_13459_구슬탈출 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(in.readLine());
