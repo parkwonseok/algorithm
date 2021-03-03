714f82ce1a86dd65406ca5d49fa749bb1680b38b
parkwonseok <48585170+parkwonseok@users.noreply.github.com>
2021-03-03 10:45:41 +0900

    Day9

---
+++ DFS & BFS/BOJ_15653_구슬탈출4.java
@@ -0,0 +1,108 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class BOJ_15653_±∏ΩΩ≈ª√‚4 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		char[][] board = new char[N][M];
+		boolean[][][][] ch = new boolean[N][M][N][M];
+		Loc start = new Loc();
+
+		for (int i = 0; i < N; i++) {
+			String row = in.readLine();
+			for (int j = 0; j < M; j++) {
+				board[i][j] = row.charAt(j);
+				if (board[i][j] == 'R') {
+					start.rx = j;
+					start.ry = i;
+				}
+				if (board[i][j] == 'B') {
+					start.bx = j;
+					start.by = i;
+				}
+			}
+		}
+
+		Deque<Loc> q = new ArrayDeque<>();
+		q.offer(start);
+		ch[start.ry][start.rx][start.by][start.bx] = true;
+		int[] dx = { 0, 1, 0, -1 };
+		int[] dy = { -1, 0, 1, 0 };
+
+		while (!q.isEmpty()) {
+			Loc cur = q.poll();
+			int bx = cur.bx;
+			int by = cur.by;
+			int rx = cur.rx;
+			int ry = cur.ry;
+			int dis = cur.dis;
+			
+			for (int i = 0; i < 4; i++) {
+				int nrx = rx, nry = ry, nbx = bx, nby = by;
+
+				while (board[nry + dy[i]][nrx + dx[i]] != '#' && board[nry][nrx] != 'O') {
+					nrx += dx[i];
+					nry += dy[i];
+				}
+				
+				while (board[nby + dy[i]][nbx + dx[i]] != '#' && board[nby][nbx] != 'O') {
+					nbx += dx[i];
+					nby += dy[i];
+				}
+				
+				if(board[nby][nbx] == 'O') continue;
+				if(board[nry][nrx] == 'O') {
+					System.out.println(dis + 1);
+					return;
+				}
+
+				if (nbx == nrx && nby == nry) {
+					if (i == 0) {
+						if (ry > by) nry++;
+						else nby++;
+					} else if (i == 1) {
+						if (rx > bx) nbx--;
+						else nrx--;
+					} else if (i == 2) {
+						if (ry > by) nby--;
+						else nry--;
+					} else {
+						if (rx > bx) nrx++;
+						else nbx++;
+					}
+				}
+
+				if (!ch[nry][nrx][nby][nbx]) {
+					ch[nry][nrx][nby][nbx] = true;
+					q.offer(new Loc(nrx, nry, nbx, nby, dis + 1));
+				}
+			}
+
+		}
+
+		System.out.println("-1");
+	}
+
+	static class Loc {
+		int rx, ry, bx, by, dis;
+
+		Loc() {
+			this.dis = 0;
+		}
+
+		Loc(int rx, int ry, int bx, int by, int dis) {
+			this.rx = rx;
+			this.ry = ry;
+			this.bx = bx;
+			this.by = by;
+			this.dis = dis;
+		}
+	}
+}
---
+++ DFS & BFS/BOJ_2210_숫자판펌프.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class BOJ_2210_¼ýÀÚÆÇÆßÇÁ {
+	static int[][] board = new int[5][5];
+	static Set<Integer> set = new HashSet<>();
+	static int[] dx = { 0, 1, 0, -1 };
+	static int[] dy = { -1, 0, 1, 0 };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		for (int i = 0; i < 5; i++) {
+			st = new StringTokenizer(in.readLine());
+			for (int j = 0; j < 5; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				DFS(j, i, 0, board[i][j]);
+			}
+		}
+		
+		System.out.println(set.size());
+	}
+
+	private static void DFS(int x, int y, int cnt, int res) {
+		if (cnt == 5) {
+			set.add(res);
+			return;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (nx < 0 || ny < 0 || nx >= 5 || ny >= 5) continue;
+
+			DFS(nx, ny, cnt + 1, res * 10 + board[ny][nx]);
+		}
+	}
+}
